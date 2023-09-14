8d14629c05ab41837ffac8dc7e536b4d5fa27af8
Aurora <aurorastorm22@proton.me>
2023-09-14 09:08:50 +0000

    Add url settings to show static files

--- knits_knots/urls.py
+++ knits_knots/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,5 @@ urlpatterns = [
     path('products/', include('products.urls',)),
     path('basket/', include('basket.urls',)),
     path('checkout/', include('checkout.urls',)),
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

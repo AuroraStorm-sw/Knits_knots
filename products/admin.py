128c20f0ef892bea8164a01c29e155c22eb1c166
Aurora <aurorastorm22@proton.me>
2023-09-18 06:04:17 +0000

    Change debug to development, testing if styling translates to heroku

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug'
+        )
+
+    prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Category)

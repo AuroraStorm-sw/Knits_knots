b5b46c2de9c8506ae17ca6948ac0b845e3626ae7
Aurora <aurorastorm22@proton.me>
2023-09-22 11:25:09 +0000

    Add form for customer checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -2,4 +2,31 @@ from django import forms
 from order.models import Order, ShippingAdress
 
 
-class OrderForm(forms.ModelForm):
+class OrderFormAddress(forms.ModelForm):
+    model = ShippingAdress
+    fields = (
+        'name',
+        'surname',
+        'email',
+        'address',
+        'city',
+        'postcode',
+        'country',
+    )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Placeholders for form input fields
+        """
+        super()-__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'surname': 'Surname',
+            'email': 'Email',
+            'adress': 'Address',
+            'city': 'City',
+            'postcode': 'Postal code',
+            'country': 'Country',
+        }
+
+    self.fields['name'].widget.attrs['autofocus'] = True

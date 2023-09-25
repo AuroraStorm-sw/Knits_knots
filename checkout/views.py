4ac70ef82da4547500706ae04435be63ff65ec7c
Aurora <aurorastorm22@proton.me>
2023-09-25 13:11:39 +0000

    Add js file for Stripe, add basic css styling

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 
 from .forms import OrderFormAddress
 
+import stripe
+
 
 def checkout(request):
     """ A view to return the checkout page """
@@ -15,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NuB5lEXoN90z8qxh1VctlmnA1Sq1qobFGgGLWyrcePkIoZZsoEueAt1u00yX66lz3tCh21thuKAKSZbGEn6gBiq00Giibjwoc',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

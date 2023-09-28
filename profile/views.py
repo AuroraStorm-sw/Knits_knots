945805e2c456ae4aa4cdc42f2e0475882c510e0a
Aurora <aurorastorm22@proton.me>
2023-09-28 10:18:23 +0000

    wire up profile with signal for automatic profile creation upon account creation with view and template

--- profile/views.py
+++ profile/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
     """ A view to return the profile page """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
-    return render(request, 'profile/profile.html')
+    context = {
+        'profile': profile,
+    }
+    return render(request, 'profile/profile.html', context)

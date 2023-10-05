cc5d1a10deb553b1dbfccc04a8ae41a7f3229033
Aurora <aurorastorm22@proton.me>
2023-10-05 10:22:20 +0000

    Add javascript to prevent the customer from adding less than 1 and more than 99 of each item

--- static/js/update-qty.js
+++ static/js/update-qty.js
@@ -1,3 +1,19 @@
+// Disable +/- buttons outside 1-99 range
+function handleEnableDisable(itemId) {
+    var currentValue = parseInt($(`#id_qty_${itemId}`).val());
+    var minusDisabled = currentValue < 2;
+    var plusDisabled = currentValue > 98;
+    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
+}
+
+// Ensure proper enabling/disabling of all inputs on page load
+var allQtyInputs = $('.qty_input');
+for(var i = 0; i < allQtyInputs.length; i++){
+    var itemId = $(allQtyInputs[i]).data('item_id');
+    handleEnableDisable(itemId);
+}
+    
     // Update quantity on click
 $('.update-link').click(function(e) {
         var form = $(this).prev('.update-form');

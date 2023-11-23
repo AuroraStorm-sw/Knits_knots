b046c2a7d565a1c76264cab53a2614a9950d9691
Aurora <aurorastorm22@proton.me>
2023-11-23 14:34:19 +0000

    Adjust company admin to show more fields, add heading to contact form

--- company/admin.py
+++ company/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Contact
 
-admin.site.register(Contact)
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'subject',
+        )

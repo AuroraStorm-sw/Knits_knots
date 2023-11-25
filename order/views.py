d8484cb2f891d0a71c272d25d1ef16d594fc0784
Aurora <aurorastorm22@proton.me>
2023-11-25 18:59:54 +0000

    Add website images to README, add roadmap.xml file

--- order/views.py
+++ order/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
 
 
-def profile(request):
-    """
-    A view to show the basket page for
-    the customer
-    """
-    return render(request, 'profile/profile.html')
